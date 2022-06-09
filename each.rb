b4118e4072a568a56aa12f680ebc9de8970230dc
Noah van Ekdom <noahvanekdom@gmail.com>
2022-06-08 20:14:08 -0600

    Add final changes

--- each.rb
+++ each.rb
@@ -3,20 +3,20 @@
   # b. Running the file in your Terminal. You can do this by running `ruby <filepath>`. If you are in this directory, you'd run `ruby each.rb`.
 
 # Challenge 1
-# Use the each method to print out a complete sentence stating the weather using the Array below:
-weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
-
-weather_options.each do |weather_option|
-  if weather_option == "snowy"
-    puts "It's a blizzard outside!"
-  end
-end
-# Challenge 2
+# # Use the each method to print out a complete sentence stating the weather using the Array below:
+# weather_options = ["sunny", "snowy", "freezing", "partly cloudy", "drizzly"]
+#
+# weather_options.each do |weather_option|
+#   if weather_option == "snowy"
+#     puts "It's a blizzard outside!"
+#   end
+# end
+# # Challenge 2
 # Use the each method to print out the words that are exactly 3 characters in length
 words = ["car", "bike", "bus", "van", "scooter", "truck", "rv"]
 words.each do |word|
   if word.length == 3
-    print word
+    puts word
   else
   end
 end
